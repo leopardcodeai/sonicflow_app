d608eb084d46551f27105ff1a2848848b99cce0c
Alexander Brunker <alexanderbrunker@Alex-MacBook-Pro.fritz.box>
2026-04-18 08:45:36 +0200

    [SF-18] Enforce zero-warning PR audits

--- sonicflow_app/ios-app/FlowTones/AudioManager.swift
+++ sonicflow_app/ios-app/FlowTones/AudioManager.swift
@@ -28,7 +28,7 @@ final class AudioManager: ObservableObject {
 
     func configureSession() {
         let session = AVAudioSession.sharedInstance()
-        try? session.setCategory(.playback, mode: .default, options: [.mixWithOthers, .allowBluetooth])
+        try? session.setCategory(.playback, mode: .default, options: [.mixWithOthers, .allowBluetoothHFP])
         try? session.setActive(true)
     }
 
